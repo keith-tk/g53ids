b3278c4a9a8168bd117fe7d6eba1451c7ac89a0a
keith-tk <tzekeat@gmail.com>
2015-02-04 04:51:25 +0800

    database test

--- index.php
+++ index.php
@@ -5,5 +5,12 @@
 	<body>
 	</body>
 	<?php echo '<p>Hello World</p>'?>
+	<?php echo "my first cloud app";
+	$link = mysql_connect('aa1h1zomr7xslqd.csmp8yhb8dto.ap-southeast-1.rds.amazonaws.com:3306', 'root', 'rootroot');
+	if (!$link) {
+	die('Could not connect: ' . mysql_error());
+	}
+	echo 'Connected successfully';
+	mysql_close($link); ?>
 	</body>
 </html>
