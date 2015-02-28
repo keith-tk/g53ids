fc82c479d6a59f06da0cda21b807d38d78ae7b3d
keith-tk <tzekeat@gmail.com>
2015-03-01 01:06:26 +0800

    first test

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+define("DB_HOST", "aa1h1zomr7xslqd.csmp8yhb8dto.ap-southeast-1.rds.amazonaws.com:3306");
+define("DB_USER", "root");
+define("DB_PASSWORD", "rootroot");
+define("DB_DATABASE", "g53ids");
+
+?>
