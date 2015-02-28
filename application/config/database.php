3f5d8c57f80f9c42f71b1616df7091547c4fa827
keith-tk <tzekeat@gmail.com>
2015-03-01 00:44:22 +0800

    static pages lesson

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
+$db['default']['hostname'] = 'aa1h1zomr7xslqd.csmp8yhb8dto.ap-southeast-1.rds.amazonaws.com:3306';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['password'] = 'rootroot';
+$db['default']['database'] = 'g53ids';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
