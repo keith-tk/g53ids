5cd3d0f4d92020f7cbc9af050d99d28c1a4b210f
keith-tk <tzekeat@gmail.com>
2015-03-07 15:50:15 +0800

    new files

--- index.php
+++ index.php
@@ -1,27 +1,4 @@
 <?php
 
-include_once 'database/config.php';
-include_once 'database/insert_functions.php';
-include_once 'database/retrieve_functions.php';
-
-$name = "Secret Recipe";
-$type = "Restaurant";
-$contact = "+60123456789";
-$openTime = "09:30:00";
-$closeTime = "08:00:00";
-$monday = '1';
-$tuesday = '1';
-$wednesday = '1';
-$thursday = '1';
-$friday = '1';
-$saturday = '0';
-$sunday = '0';
-$latitude = '2.945219';
-$longitude = '101.874778';
-
-// $database = new Insert_Functions();
-// $database->addNewInsertion($name, $type, $contact, $openTime, $closeTime, $monday, $tuesday, $wednesday, $thursday, $friday, $saturday, $sunday, $latitude, $longitude);
-// $database = new Retrieve_Functions();
-// $database->getAllRecords();
 echo "Hello World";
 ?>
