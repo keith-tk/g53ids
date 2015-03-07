881cc315285e753290fae00f4fc20efd7c7f2b6f
keith-tk <tzekeat@gmail.com>
2015-03-07 16:47:01 +0800

    test sync

---
+++ requestSync.php
@@ -0,0 +1,20 @@
+
+<?php
+
+$url = 'https://g53ids-env.elasticbeanstalk.com/syncDatabase.php';
+$data = array('date' => '2015-02-07 08:24:37');
+
+// use key 'http' even if you send the request to https://...
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data),
+    ),
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+
+var_dump($result);
+
+?>
